c2d10996aadb7935b3bf0648038e125b8f26a388
myxy <m.myxomatosis@gmail.com>
2021-08-18 07:23:21 +0900

    基礎部分を作成

---
+++ Assets/EnvironmentManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnvironmentManager : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] private Material skyMaterial;
+    [Serializable]
+    public class EnvironmentParameter
+    {
+        [SerializeField] public Color SkyColor;
+    }
+
+    [SerializeField] private EnvironmentParameter[] parameters;
+    [SerializeField] private int index;
+    public void Refresh()
+    {
+        skyMaterial.SetColor("_Color2", parameters[index].SkyColor);
+    }
+#endif
+}
---
+++ Assets/Tools/Editor/EnvironmentManagerEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EnvironmentManager))]
+public class EnvironmentManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Refresh"))
+        {
+            (target as EnvironmentManager).Refresh();
+        }
+    }
+}
